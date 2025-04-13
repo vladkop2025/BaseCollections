2b71de9c9a30660d7e730204fc042e9dba2b738f
vladkop2025 <vladkop@mail.ru>
2025-04-13 23:04:24 +0300

    branch2

--- BaseCollections/Program.cs
+++ BaseCollections/Program.cs
@@ -27,6 +27,41 @@ namespace BaseCollections
                     List<string> words = new List<string>(noPunctuationText.Split(new[] { ' ', '\n', '\r', '\t' },
                                                  StringSplitOptions.RemoveEmptyEntries));
 
+                    // Создаем словарь для подсчета частоты слов
+                    Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+                    foreach (var word in words)
+                    {
+                        if (word.Length <= 2) continue; // Пропускаем короткие слова
+
+                        if (wordFrequency.ContainsKey(word))
+                        {
+                            wordFrequency[word]++;
+                        }
+                        else
+                        {
+                            wordFrequency[word] = 1;
+                        }
+                    }
+
+                    // Преобразуем словарь в список для сортировки
+                    List<KeyValuePair<string, int>> sortedWords = new List<KeyValuePair<string, int>>(wordFrequency);
+
+                    // Сортируем по убыванию частоты
+                    sortedWords.Sort((a, b) => b.Value.CompareTo(a.Value));
+
+                    // Выводим топ-10 слов
+                    Console.WriteLine("Топ-10 самых частых слов:");
+                    Console.WriteLine("--------------------------");
+                    Console.WriteLine("Слово\t\tКоличество");
+                    Console.WriteLine("--------------------------");
+
+                    int count = 0;
+                    foreach (var pair in sortedWords)
+                    {
+                        if (count++ >= 10) break;
+                        Console.WriteLine($"{pair.Key}\t\t{pair.Value}");
+                    }
+
                     // Останавливаем таймер
                     stopwatch.Stop();
 
@@ -59,6 +94,21 @@ namespace BaseCollections
     }
 }
 
+//Топ - 10 самых частых слов:
+//--------------------------
+//Слово           Количество
+//--------------------------
+//что             1948
+//как             1304
+//она             1244
+//Обломов         911
+//его             886
+//все             869
+//это             735
+//так             715
+//сказал          548
+//только          546
+
 //Найдено 147853 слов:
-//Время обработки: 63 мс
+//Время обработки: 78 мс
 //---------------------- -
