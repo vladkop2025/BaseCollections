0a48585587702bddcef74532312a88293346ba78
vladkop2025 <vladkop@mail.ru>
2025-04-14 07:45:32 +0300

    branch1

--- BaseCollections/Program.cs
+++ BaseCollections/Program.cs
@@ -16,7 +16,10 @@ namespace BaseCollections
             // Создаем и запускаем таймер
             Stopwatch stopwatch = Stopwatch.StartNew();
 
-            string filePath = "C:\\Users\\vladk\\Desktop\\Text.txt";
+            //string filePath = "C:\\Users\\vladk\\Desktop\\Text.txt";
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //string filePath = Path.Combine("C:", "Users", "vladk", "Desktop", "Text.txt");
+            string filePath = Path.Combine(desktopPath, "Text.txt");
             if (File.Exists(filePath))
             {
                 try
